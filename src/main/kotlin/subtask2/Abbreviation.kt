2571b70b640c7653b12dfa6aa1bc55d7dce7001f
AndreyYurkevich <58786656+AndreyYurkevich@users.noreply.github.com>
2021-06-16 22:38:31 +0300

    feat: fix bugs

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -1,13 +1,28 @@
 package subtask2
 
+import java.util.*
+
 class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        val A = arrayOf(a.toUpperCase())
-        val B  = arrayOf(b)
-        return if (A.intersect(B.toList()) == B) {
-             "YES"
-        } else "NO"
-    }
-}
+        val A = a.toUpperCase()
+        val B = b.toCharArray()
+        var count = 0
+       // println(B.size)
+        while (count<B.size){
+            if (A.indexOf(B.get(count).toString(),count,false) >= 0){
+                count++
+                if (count == B.size) return "YES"
+               }else return "NO"
+            }
+
+ return "NO"      }
+
+ }
+
+
+
+
+
+
