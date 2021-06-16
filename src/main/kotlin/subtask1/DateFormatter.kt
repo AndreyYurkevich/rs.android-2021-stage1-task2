7fb10220d937f6f74b7b7f8adbb67b059ba58135
AndreyYurkevich <58786656+AndreyYurkevich@users.noreply.github.com>
2021-06-16 20:49:04 +0300

    feat : update task

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,27 @@
 package subtask1
 
+
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+
 class DateFormatter {
 
     // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        // throw NotImplementedError("Not implemented")
+        val Text = "Такого дня не существует"
+           return try {
+
+             val  formatDate = DateTimeFormatter.ofPattern("dd MMMM, EEEE", Locale.forLanguageTag("Ru"))
+             LocalDate.of(year.toInt(),month.toInt(),day.toInt()).format(formatDate)
+
+        } catch (E: Exception){
+            Text
+        }
+
     }
 }
--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -4,6 +4,10 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        val A = arrayOf(a.toUpperCase())
+        val B  = arrayOf(b)
+        return if (A.intersect(B.toList()) == B) {
+             "YES"
+        } else "NO"
     }
 }
